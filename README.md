c169370afdd842f0c134cdab39dc7c184d614fb1
kumarRishabh16 <kumarrishabhrudeboy@gmail.com>
2022-09-28 12:48:52 +0530

    home.html

--- README.md
+++ README.md
@@ -18,3 +18,22 @@ conda create -p venv python==3.10.6 -y
 ```
 conda activate venv 
 ```
+##JSON Data format to check on postman
+
+```
+"data": {
+    "CRIM": 0.00632,
+    "ZN": 18.0,
+    "INDUS": 2.31,
+    "CHAS": 0.0,
+    "NOX": 0.538,
+    "RM": 6.575,
+    "AGE": 65.2,
+    "DIS": 4.09,
+    "RAD": 1.0,
+    "TAX": 296.0,
+    "PTRATIO": 15.3,
+    "B": 396.9,
+    "LSTAT": 4.98
+    }
+```
--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import json
 import pickle
-from flask import Flask, request, jsonify, app, render_template, url_for
+
+from flask import Flask,request,app,jsonify, url_for, render_template
 import numpy as np
-import pandas as pd 
+import pandas as pd
+
 
 app = Flask(__name__)
 
 # Load the model
-model=pickle.load(open('regmodel.pkl','rb'))
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -17,15 +21,18 @@ def home():
 
 '''
 
-@app.route('/predict_api', method=['POST']):
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1-1))    output=regmodel.predict(new_data)
-    print(output)
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
     return jsonify(output[0])
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
